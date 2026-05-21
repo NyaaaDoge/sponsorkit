bda6698f4cc336f8651d1e00bc297a9133bbf912
nyaaadoge <nyaaadoge@gmail.com>
2026-05-21 20:53:53 +0800

    chore: simplify sponsor display order

--- sponsor.config.ts
+++ sponsor.config.ts
@@ -10,31 +10,36 @@ export default defineConfig({
   outputDir: './sponsorkit',
   width: 800,
   formats: ['json', 'svg', 'png'],
+  includePastSponsors: true,
 
-  tiers: [
-    {
-      title: '历史赞助者',
-      monthlyDollars: -1,
-      preset: tierPresets.xs,
-    },
-    {
-      title: '赞助者',
-      preset: tierPresets.base,
-    },
-    {
-      title: '30 元以上',
-      monthlyDollars: 30,
-      preset: tierPresets.medium,
-    },
-    {
-      title: '50 元以上',
-      monthlyDollars: 50,
-      preset: tierPresets.large,
-    },
-    {
-      title: '100 元以上',
-      monthlyDollars: 100,
-      preset: tierPresets.xl,
-    },
-  ],
+  customComposer: async (composer, sponsors) => {
+    const activeSponsors = sponsors
+      .filter(sponsor => sponsor.monthlyDollars > 0)
+      .sort((a, b) => b.monthlyDollars - a.monthlyDollars)
+
+    const pastSponsors = sponsors
+      .filter(sponsor => sponsor.monthlyDollars <= 0)
+      .sort((a, b) => Date.parse(b.createdAt || '') - Date.parse(a.createdAt || ''))
+
+    composer.addSpan(20)
+
+    if (activeSponsors.length) {
+      composer
+        .addTitle('赞助者')
+        .addSpan(10)
+
+      await composer.addSponsorGrid(activeSponsors, tierPresets.medium)
+    }
+
+    if (pastSponsors.length) {
+      composer
+        .addSpan(20)
+        .addTitle('历史赞助者')
+        .addSpan(10)
+
+      await composer.addSponsorGrid(pastSponsors, tierPresets.xs)
+    }
+
+    composer.addSpan(20)
+  },
 })
